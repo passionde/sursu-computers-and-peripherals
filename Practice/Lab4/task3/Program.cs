0ab80387ecf65b3b0909ed463f7d212be4ac77a5
passionde <yarikdev@bk.ru>
2023-12-08 00:03:21 +0500

    add: task4, task5

---
+++ Practice/Lab4/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using Iot.Device.OneWire;
+
+bool flag = true;
+
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    flag = false;
+};
+
+while (flag)
+{
+    foreach (var dev in OneWireThermometerDevice.EnumerateDevices())
+    {
+        Console.WriteLine($"Name: {dev.DeviceId}");
+        var temp = await dev.ReadTemperatureAsync();
+        Console.WriteLine($"Temperature: {temp.DegreesCelsius:g}\n");
+    }
+}
+
